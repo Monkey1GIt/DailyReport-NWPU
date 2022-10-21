a4534effd042e6aac5ef5efd7d603ba5b92d299f
Monkey CN <43908098+Monkey1GIt@users.noreply.github.com>
2022-10-21 13:02:05 +0800

    update: update the signin function 1. fix the script to adapt to the newly website
    test: run correctly in windows and raspiberry

--- main.py
+++ main.py
@@ -2,18 +2,19 @@ import json
 import logging
 import os
 import re
+import sys
 
 import requests
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-# 打开配置文件
-with open('User/Userinfo.json', 'r', encoding='utf-8') as f:
-    USERINFO = json.load(f)
 
+FLAG_DEBUG = False
 URL = 'http://yqtb.nwpu.edu.cn/wx/xg/yz-mobile/index.jsp'
-USERNAME = USERINFO['USERNAME']
-PASSWORD = USERINFO['PASSWORD']
-
+# 输入传递参数
+USERNAME = sys.argv[1]
+PASSWORD = sys.argv[2]
 # logging配置
 log_format = '[%(asctime)s] - %(levelname)s\t> %(message)s'
 log_filename = 'opt.log'
@@ -22,44 +23,52 @@ logging.basicConfig(level=logging.INFO,
                     format=log_format, handlers=log_hander)
 
 
-def get_cookies():
+def submit_form():
     status = 'NO'
     info = ''
     # Chrome浏览器
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument('--headless')
-    driver = webdriver.Chrome(chrome_options=chrome_options)
+    user_options = webdriver.ChromeOptions()
+    if not FLAG_DEBUG:
+        user_options.add_argument('--headless')
+    driver = webdriver.Chrome(options=user_options)
     # 打开网页
     driver.get(URL)
     # 填写表单
-    form_username = driver.find_element_by_id('username')
+    form = driver.find_element_by_xpath('//li[contains(text(),"密码")]')
+    form.click()
+    form_username = driver.find_element_by_xpath(
+        '//form[@id="fm1"]//input[@id="username"]')
     form_username.send_keys(USERNAME)
-    form_password = driver.find_element_by_id('password')
+    form_password = driver.find_element_by_xpath(
+        '//form[@id="fm1"]//input[@id="password"]')
     form_password.send_keys(PASSWORD)
     # 关闭弹窗
     # driver.find_element_by_xpath("//button[@class='el-dialog__headerbtn']").click()
     # 提交
-    driver.find_element_by_xpath("//input[@name='submit']").click()
-    try:
-        # 获取重定向的url
-        driver.find_element_by_xpath("//i[@class='icon iconfont icon-shangbao1']").click()
-    except Exception as e:
-        # msg_error = 'get_cookies: ' + e.__class__.__name__ + ': ' + str(e)
-        if e.__class__.__name__.find('NoSuchElementException') > -1:
-            if driver.page_source.find('账号或密码错误') > -1:
-                info = '账号或密码错误'
-            else:
-                info = driver.page_source[:200]
-            return status, info
-    # 
+    btn_submit = driver.find_element_by_xpath(
+        '//input[@name="button" and @accesskey="l"]')
+    btn_submit.click()
+    # 显示等待页面跳转
+    WebDriverWait(driver, 3).until(EC.title_is('疫情每日填报'))
+    # 查询登录状态
+    if driver.page_source.find('疫情每日填报') == -1:
+        # msg_error = 'submit_form: ' + e.__class__.__name__ + ': ' + str(e)
+        if driver.page_source.find('账号或密码错误') > -1:
+            info = '账号或密码错误'
+        else:
+            info = driver.page_source[:200]
+        return status, info
+    # 检查提交状态
     if driver.page_source.find('您已提交今日填报') > -1:
         status = 'OK'
         info = 'Have Signed!'
         return status, info
     # 模拟点击
-    driver.find_element_by_xpath("//a[@class='weui-btn weui-btn_primary']").click()
-    driver.find_element_by_xpath("//i[@class='weui-icon-checked']").click()
-    driver.find_element_by_xpath("//a[@id='save_div']").click()
+    driver.find_element_by_xpath(
+        '//a[@class="weui-btn weui-btn_primary"]').click()
+    driver.find_element_by_xpath(
+        '//label[@class="weui-cell weui-cell_active weui-check__label"]').click()
+    driver.find_element_by_xpath('//a[@id="save_div"]').click()
     # 判断状态
     driver.get('https://yqtb.nwpu.edu.cn/wx/ry/jrsb_xs.jsp')
     if driver.page_source.find('您已提交今日填报') > -1:
@@ -74,7 +83,7 @@ def get_cookies():
 
 
 if __name__ == '__main__':
-    status, info = get_cookies()
+    status, info = submit_form()
     # log
     out_text = f'NWPU每日疫情填报: {status}: {info}'
     logging.info(out_text)
